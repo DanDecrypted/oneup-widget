492daa57d3e4ec95c03d5d799a7154971bf4aa4e
Dan Oak <coderdan@protonmail.com>
2024-01-22 00:36:04 +0000

    use event handler instead of useEffect

--- src/components/CommissionWidget.tsx
+++ src/components/CommissionWidget.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import CommissionApi from "../services/CommissionApi";
 import "./CommissionWidget.css"
 import CommissionBand from "../types/CommissionBand";
@@ -17,8 +17,9 @@ const CommissionWidget = () => {
     setCommissionBands(new CommissionApi().getCommissionBands());
   }, []);
 
-  useEffect(() => {
-    if (isNaN(enteredRevenue) || enteredRevenue === 0) {
+  const onRevenueChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const inputValue = parseInt(e.target.value);
+    if (isNaN(inputValue) || inputValue === 0) {
       setCommissionBands(new CommissionApi().getCommissionBands());
       setEnteredRevenue(0);
       setEarnedCommission(0);
@@ -26,11 +27,12 @@ const CommissionWidget = () => {
       return;
     }
 
+    setEnteredRevenue(inputValue);
     const remappedCommissionBands = commissionBands.map(band => {
-      if (band.lowerBand === undefined || enteredRevenue >= band.lowerBand) {
+      if (band.lowerBand === undefined || inputValue >= band.lowerBand) {
         const chunk = band.upperBand === undefined ? 
-          enteredRevenue - (band.lowerBand || 0) : 
-          (band.upperBand > enteredRevenue ? enteredRevenue : band.upperBand)  - (band.lowerBand || 0); 
+        inputValue - (band.lowerBand || 0) : 
+          (band.upperBand > inputValue ? inputValue : band.upperBand)  - (band.lowerBand || 0); 
 
         return {...band, chunk, commission: chunk * band.percentage}
       }
@@ -38,12 +40,11 @@ const CommissionWidget = () => {
     });
 
     setCommissionBands(remappedCommissionBands);
-    console.log(remappedCommissionBands);
 
     const finalCommission  = remappedCommissionBands.map(c => c.commission).reduce((prev, curr) => prev + curr, 0);
     setEarnedCommission(finalCommission);
-    setEarnedCommissionPercentage((finalCommission / enteredRevenue) * 100);
-  }, [enteredRevenue])
+    setEarnedCommissionPercentage((finalCommission / inputValue) * 100);
+  }
 
   return <>
     <div className="card commission-card">
@@ -52,7 +53,7 @@ const CommissionWidget = () => {
         <span className="title">Deal Commission</span>
         <div className="input-group">
           <label htmlFor="revenue">Revenue:</label>
-          <input type="number" id="revnue" onChange={(e) => setEnteredRevenue(parseInt(e.target.value))} />
+          <input type="number" id="revenue" onChange={onRevenueChange} />
         </div>
       </div>
       <div className="body d-flex fd-row">
@@ -60,7 +61,7 @@ const CommissionWidget = () => {
           <CircularProgressbarWithChildren 
             value={earnedCommission}
             minValue={0}
-            maxValue={enteredRevenue == 0 ? 100 : enteredRevenue}
+            maxValue={enteredRevenue === 0 ? 100 : enteredRevenue}
             styles={buildStyles({
               pathColor: '#17991a'
             })}
